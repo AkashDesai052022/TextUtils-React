c4288052c4a9d13e7f286d4127df1eff44c8c0c5
AkashDesai052022 <akashdesai052022@gmail.com>
2024-11-01 17:37:42 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,16 +25,16 @@ function TextForm(props) {
             <div className="mb-3">
             <textarea className="form-control" value = {text} onChange={handleOnChange} style={{backgroundColor:props.mode ==='light'?'white':'#1e272e', color: props.mode ==="dark"?"white":"#1e272e"}} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn-btn-primary mx-2" onClick={handleUpClick} >Convert to UpperCase</button>
-            <button className="btn-btn-primary mx-2" onClick={handleLowClick} >Convert to LowerCase</button>
-            <button className="btn-btn-primary mx-2" onClick={clearText} >Clear Text</button>
+            <button disabled={text.length===0} className="btn-btn-primary mx-2" onClick={handleUpClick} >Convert to UpperCase</button>
+            <button disabled={text.length===0} className="btn-btn-primary mx-2" onClick={handleLowClick} >Convert to LowerCase</button>
+            <button disabled={text.length===0} className="btn-btn-primary mx-2" onClick={clearText} >Clear Text</button>
        </div>
        <div className='container my-2' style={{color:props.mode ==="dark"?"white":"#1e272e "}}>
             <h2> Your Text Summery</h2>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} minutes require to read</p>
             <h2>Preview</h2>
-            <p>{text ===""?'Enter text in Textarea for preview': text}</p>
+            <p>{text.length>0?text:'Nothing to preview!'}</p>
        </div>
         </> 
     )
